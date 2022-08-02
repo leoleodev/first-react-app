c55037ea5bfac49dcdac3adfb628db159420405f
Leonardo <leonardoj.m.4@gmail.com>
2022-08-01 23:00:37 -0300

    Estilização do componente

--- src/components/ShowButtons.tsx
+++ src/components/ShowButtons.tsx
@@ -1,9 +1,11 @@
 
+import Styles from '../styles/modules/ShowButtons.module.css';
+
 export function ShowButtons(){
     return(
-        <button type="button">
+        <button type="button" className={Styles.buttonTeste}>
             Button de
-            <strong>Teste</strong>
-        </button>
+            <strong> Teste</strong>
+        </button>        
     );
 }
